833529a873ac6a847d8a02a1d7321cb46439343d
Dana Paulette Perkins <dana.p.perkins@gmail.com>
2024-08-05 18:26:14 -0500

    Added Github url and netlify to footer

--- src/App.js
+++ src/App.js
@@ -3,7 +3,30 @@ import './App.css';
 export default function App() {
   return (
     <div className="App">
-      Weather App
+      <h1>Weather App</h1>
+      <footer>
+        This project was coded by{" "}
+        <a
+          href="https://www.shecodes.io/"
+          target="_blank"
+          rel="noopener noreferrer">
+          SheCodes
+        </a>{" "}
+        and is{" "}
+        <a
+          href="https://github.com/dperki02/react-weather-app"
+          target="_blank"
+          rel="noopener noreferrer">
+          open-sourced on GitHub
+        </a>{" "}
+        and{" "}
+        <a
+          href="https://react-weather-app-dpp.netlify.app"
+          target="_blank"
+          rel="noopener noreferrer">
+          hosted on Netlify
+        </a>
+      </footer>
     </div>
   );
 }
