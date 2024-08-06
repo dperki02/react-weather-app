0c69749b87a08abbf09bb149f7c3f093f10aa0a9
Dana Paulette Perkins <dana.p.perkins@gmail.com>
2024-08-05 20:20:16 -0500

    imported bootstrap, html structure

--- src/App.js
+++ src/App.js
@@ -1,32 +1,36 @@
 import './App.css';
+import Weather from "./Weather";
 
 export default function App() {
   return (
     <div className="App">
-      <h1>Weather App</h1>
-      <footer>
-        This project was coded by{" "}
-        <a
-          href="https://www.shecodes.io/"
-          target="_blank"
-          rel="noopener noreferrer">
-          SheCodes
-        </a>{" "}
-        and is{" "}
-        <a
-          href="https://github.com/dperki02/react-weather-app"
-          target="_blank"
-          rel="noopener noreferrer">
-          open-sourced on GitHub
-        </a>{" "}
-        and{" "}
-        <a
-          href="https://react-weather-app-dpp.netlify.app"
-          target="_blank"
-          rel="noopener noreferrer">
-          hosted on Netlify
-        </a>
-      </footer>
+      <div className="container">
+        
+        <Weather />
+        <footer>
+          This project was coded by{" "}
+          <a
+            href="https://www.shecodes.io/"
+            target="_blank"
+            rel="noopener noreferrer">
+            SheCodes
+          </a>{" "}
+          and is{" "}
+          <a
+            href="https://github.com/dperki02/react-weather-app"
+            target="_blank"
+            rel="noopener noreferrer">
+            open-sourced on GitHub
+          </a>{" "}
+          and{" "}
+          <a
+            href="https://react-weather-app-dpp.netlify.app"
+            target="_blank"
+            rel="noopener noreferrer">
+            hosted on Netlify
+          </a>
+        </footer>
+      </div>
     </div>
   );
 }
